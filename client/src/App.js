72f3e9e02f2a2c0336882819f32b817de3e0a9cd
gaspar <gasparmunoz@hotmail.com>
2022-08-18 16:29:07 -0300

    NavBar finalizada y agregue path /inicio para navB

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div className="App">
       <Routes>
         <Route exact path={"/"} element={<Landing />} />
+        <Route path={"/inicio"} element={<NavBar />} />
       </Routes>
     </div>
   );
