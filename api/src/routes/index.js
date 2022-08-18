77a5343ca463355ddbc289cd5660f678dfcb7238
CAROLMEJIA <mejia.zapata.carolina@gmail.com>
2022-08-18 12:46:01 -0500

    Routes, controller activities

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,5 +1,9 @@
 const { Router } = require("express");
+const activity = require("./activities.routes.js")
 
 const router = Router();
+
+router.use("/activity", activity);
+router.use("/activities", activity);
 
 module.exports = router;
