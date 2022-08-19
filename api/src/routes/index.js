cf67a91efdbe42a443cea31dbf15eef01a35c313
CAROLMEJIA <mejia.zapata.carolina@gmail.com>
2022-08-18 19:36:04 -0500

    creacion ruta classpass, controller y modificacion al archivo activities.controller

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -3,13 +3,14 @@ const gym = require("./gym.routes");
 const user = require("./user.routes.js");
 const activity = require("./activities.routes.js");
 const professional = require("./professional.router");
+const classpass = require("./classpass.routes.js");
 const router = Router();
 
 router.use('/gym', gym);
 router.use("/user", user);
 router.use("/activity", activity);
-router.use("/activities", activity);
 router.use('/professional', professional);
+router.use('/classpass', classpass);
 
 
 
