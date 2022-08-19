3fb465275533c2360f3430b9544c7481dccd6c06
mishino123 <jicorreac@unal.edu.co>
2022-08-19 09:06:49 -0500

    dos

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
+import NavBar from "./components/NavBar";
 
 
 function App() {
@@ -9,7 +10,11 @@ function App() {
     <div className="App">
       <Routes>
         <Route exact path={"/"} element={<Landing />} />
+<<<<<<< HEAD
        
+=======
+        <Route path={"/inicio"} element={<NavBar />} />
+>>>>>>> 1842e77eb85f6342e6ccd78fb65ddac2e0670b31
       </Routes>
     </div>
   );
