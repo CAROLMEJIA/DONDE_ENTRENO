6a46aea2307c69836ff0e50adaa21ab3e9797ae4
Agustin Viera <facundoviera98@gmail.com>
2022-08-18 23:09:45 -0300

    New Home, Nav y Landing tocadas

--- client/src/App.js
+++ client/src/App.js
@@ -3,13 +3,15 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Landing from "./components/Landing";
 import NavBar from "./components/NavBar";
+import Home from './components/Home';
 
 function App() {
   return (
-    <div className="App">
+    <div className = 'App'>
       <Routes>
-        <Route exact path={"/"} element={<Landing />} />
-        <Route path={"/inicio"} element={<NavBar />} />
+        <Route exact path = {"/"} element = {<Landing />} />
+        <Route path = {"/inicio"} element = {<NavBar />} />
+        <Route exact path = {'/inicio/home'} element = {<Home/>} />
       </Routes>
     </div>
   );
