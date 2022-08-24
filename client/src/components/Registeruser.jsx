aedcc3ecba6f207809611b32584ee6d8ab3c68f2
gaspar <gasparmunoz@hotmail.com>
2022-08-24 11:24:09 -0300

    corrigiendo errores de import

--- client/src/components/Registeruser.jsx
+++ client/src/components/Registeruser.jsx
@@ -1,9 +1,9 @@
 import React, { useEffect } from "react";
-import "./estilos/RegisterUser.css";
+import "./estilos/Registeruser.css";
 import { useState } from "react";
-import GoogleLogin from 'react-google-login';
+import GoogleLogin from "react-google-login";
 import { gapi } from "gapi-script";
-import FacebookLogin from 'react-facebook-login';
+import FacebookLogin from "react-facebook-login";
 import { useDispatch } from "react-redux";
 import { postRegister } from "../redux/actions";
 
@@ -13,106 +13,144 @@ export function validate(register) {
     error.name = "*name is required*";
   } else if (/\d/.test(register.name)) {
     error.name = "invalid, not included number";
-  } if (!register.mail) {
+  }
+  if (!register.mail) {
     error.mail = "*mail is required*";
   }
-  return error
+  return error;
 }
 
 export default function Registeruser() {
-
   const dispatch = useDispatch();
-  const [error, setError] = useState({})
+  const [error, setError] = useState({});
   const [register, setRegister] = useState({
     name: "",
     mail: "",
     usuario: "",
     password: "",
     connected: false,
-  })
+  });
 
   function handleChange(e) {
-    setRegister({ ...register, [e.target.name]: e.target.value })
-    let objetovalidate = validate({ ...register, [e.target.name]: e.target.value })
-    setError(objetovalidate)
+    setRegister({ ...register, [e.target.name]: e.target.value });
+    let objetovalidate = validate({
+      ...register,
+      [e.target.name]: e.target.value,
+    });
+    setError(objetovalidate);
   }
 
   function handleChangeconnected(e) {
     if (register.connected == "on") {
-      setRegister({ ...register, [e.target.name]: false })
-    }
-    else {
-      setRegister({ ...register, [e.target.name]: e.target.value })
+      setRegister({ ...register, [e.target.name]: false });
+    } else {
+      setRegister({ ...register, [e.target.name]: e.target.value });
     }
   }
 
   function sendUser(e) {
-    e.preventDefault(e)
-    dispatch(postRegister(register))
+    e.preventDefault(e);
+    dispatch(postRegister(register));
     setRegister({
       name: "",
       mail: "",
       usuario: "",
       password: "",
       connected: false,
-    })
+    });
   }
-
 
   return (
     <div className="containeruser">
       <div className="containerregister">
-        <div className="image">
-        </div>
+        <div className="image"></div>
         <div className="userform">
           <h2 className="fw-bold text-center py-4">Registrarse</h2>
           <form className="formgroup" onSubmit={sendUser}>
             <div className="mb-2.5">
+              <label htmlFor="name" className="form-label">
+                Nombre Completo
+              </label>
+              <input
+                type="text"
+                placeholder="ingresa tu nombre"
+                name="name"
+                value={register.name}
+                className="form-control"
+                onChange={handleChange}
+              ></input>
+              <div className="valid-feedback">¡Campo obligatorio! |</div>
 
-              <label htmlFor="name" className="form-label">Nombre Completo</label>
-              <input type="text" placeholder="ingresa tu nombre" name="name" value={register.name} className="form-control" onChange={handleChange}></input>
-              <div className="valid-feedback">
-                ¡Campo obligatorio!
-                |</div>
-
-              {error.name && (<p >{error.name}</p>)}
-
+              {error.name && <p>{error.name}</p>}
             </div>
 
             <div className="mb-2">
-              <label htmlFor="correo" className="form-label">Correo electronico</label>
-              <input type="email" placeholder="ingresa tu correo electronico" name="mail" value={register.mail} className="form-control" onChange={handleChange}></input>
-              {error.mail && (<p >{error.mail}</p>)}
+              <label htmlFor="correo" className="form-label">
+                Correo electronico
+              </label>
+              <input
+                type="email"
+                placeholder="ingresa tu correo electronico"
+                name="mail"
+                value={register.mail}
+                className="form-control"
+                onChange={handleChange}
+              ></input>
+              {error.mail && <p>{error.mail}</p>}
             </div>
 
             <div className="mb-2">
-
-              <label htmlFor="user" className="form-label">Nombre de usuario</label>
-              <input type="text" placeholder="ingresa tu alias" name="usuario" className="form-control" value={register.usuario} onChange={handleChange}></input>
+              <label htmlFor="user" className="form-label">
+                Nombre de usuario
+              </label>
+              <input
+                type="text"
+                placeholder="ingresa tu alias"
+                name="usuario"
+                className="form-control"
+                value={register.usuario}
+                onChange={handleChange}
+              ></input>
             </div>
             <div className="mb-4">
-              <label htmlFor="password" className="form-label">Contraseña</label>
-              <input type="password" placeholder="ingresa tu password" name="password" value={register.password} className="form-control" onChange={handleChange}></input>
+              <label htmlFor="password" className="form-label">
+                Contraseña
+              </label>
+              <input
+                type="password"
+                placeholder="ingresa tu password"
+                name="password"
+                value={register.password}
+                className="form-control"
+                onChange={handleChange}
+              ></input>
             </div>
             <div className="containerbutton">
               <div className="mb-4">
-                <input type="checkbox" name="connected" className="form-check-input" onChange={handleChangeconnected} />
-                <label htmlFor="connected" className="form-check-label">Mantenerme conectado</label>
+                <input
+                  type="checkbox"
+                  name="connected"
+                  className="form-check-input"
+                  onChange={handleChangeconnected}
+                />
+                <label htmlFor="connected" className="form-check-label">
+                  Mantenerme conectado
+                </label>
               </div>
               <div>
-                <button type="submit" disabled={(error.name && error.mail) ? true : false} className="button">registrarse</button>
+                <button
+                  type="submit"
+                  disabled={error.name && error.mail ? true : false}
+                  className="button"
+                >
+                  registrarse
+                </button>
               </div>
             </div>
           </form>
-          <div className="container">
-          </div>
-
+          <div className="container"></div>
         </div>
       </div>
-
-
     </div>
-
-
   );
 }
--- client/src/components/Userform.jsx
+++ client/src/components/Userform.jsx
@@ -1,159 +1,179 @@
-import React,{useEffect,useState} from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
-import "./estilos/UserForm.css";
-import FacebookLogin from 'react-facebook-login';
-import GoogleLogin from 'react-google-login';
-import {gapi} from "gapi-script";
-import {useDispatch} from "react-redux";
+import "./estilos/Userform.css";
+import FacebookLogin from "react-facebook-login";
+import GoogleLogin from "react-google-login";
+import { gapi } from "gapi-script";
+import { useDispatch } from "react-redux";
 import { userLogin } from "../redux/actions";
 
-
-  
-  export function validate(login){
-	const error={};
-	if(!login.name){
-		error.name="*name is required*";
-	}
-	if(!login.password){
-	  error.password="*password is required*";
-	}
-	return error
-	}
-
-  const Login=()=>{
-    
-	const clientId="704047841570-i6n8lvda54ako40u5173qcd8os0qfphk.apps.googleusercontent.com";
-   useEffect(()=>{gapi.load("client.auth2",()=>{gapi.auth2.init({clientId:clientId})})},[]) }
-
-
-export default function Userform() {
-	const dispatch=useDispatch();
-    const [error,setError]=useState({})
-	const [login,setLogin]=useState({
-		name:"",
-		password:"",
-		email:""
-	})
-	function handleChange(e){
-		setLogin({...login,[e.target.name]:e.target.value})
-		let objetovalidate=validate({...login,[e.target.name]:e.target.value})
-         setError(objetovalidate)
-	}
-
-
-  function handleLogin(e){
-	e.preventDefault(e)
-	
-	dispatch(userLogin(login))
-	setLogin({
-		name:"",
-		password:"",
-	  })
-	
+export function validate(login) {
+  const error = {};
+  if (!login.name) {
+    error.name = "*name is required*";
   }
-  const responseFacebook = (responsef) => {
-	if(responsef.name){
-	const loginfb={
-		name:responsef.name,
-		passsword:responsef.userID,
-		email: responsef.email,	
-	}
-	dispatch(userLogin(loginfb))
+  if (!login.password) {
+    error.password = "*password is required*";
+  }
+  return error;
 }
 
+const Login = () => {
+  const clientId =
+    "704047841570-i6n8lvda54ako40u5173qcd8os0qfphk.apps.googleusercontent.com";
+  useEffect(() => {
+    gapi.load("client.auth2", () => {
+      gapi.auth2.init({ clientId: clientId });
+    });
+  }, []);
+};
 
-	
+export default function Userform() {
+  const dispatch = useDispatch();
+  const [error, setError] = useState({});
+  const [login, setLogin] = useState({
+    name: "",
+    password: "",
+    email: "",
+  });
+  function handleChange(e) {
+    setLogin({ ...login, [e.target.name]: e.target.value });
+    let objetovalidate = validate({
+      ...login,
+      [e.target.name]: e.target.value,
+    });
+    setError(objetovalidate);
   }
+
+  function handleLogin(e) {
+    e.preventDefault(e);
+
+    dispatch(userLogin(login));
+    setLogin({
+      name: "",
+      password: "",
+    });
+  }
+  const responseFacebook = (responsef) => {
+    if (responsef.name) {
+      const loginfb = {
+        name: responsef.name,
+        passsword: responsef.userID,
+        email: responsef.email,
+      };
+      dispatch(userLogin(loginfb));
+    }
+  };
 
   const responseGoogle = (response) => {
-	console.log(response.profileObj)
-	if(response.profileObj){
-		const loginGoogle={
-			name:response.profileObj.name,
-			passsword:response.profileObj.userID,
-			email: response.profileObj.email,	
-		}
-		dispatch(userLogin(loginGoogle))
-  }
-
-  }
-
-
+    console.log(response.profileObj);
+    if (response.profileObj) {
+      const loginGoogle = {
+        name: response.profileObj.name,
+        passsword: response.profileObj.userID,
+        email: response.profileObj.email,
+      };
+      dispatch(userLogin(loginGoogle));
+    }
+  };
 
   return (
-   <div  className="containerform">
-    <div class="container">
-	<div class="d-flex justify-content-center h-100">
-		<div class="card">
-			<div class="card-header">
-				<h3>Ingresa</h3>
-			
-			</div>
-			<div class="cardbody" >
-				<form onSubmit={handleLogin}>
-					<div className="input-group form-group">
-						<div className="input-group-prepend">
-							<span className="input-group-text"><i className="fas fa-user"></i></span>
-						</div>
-						
-						<input type="text" className="form-control" placeholder="email" name="name" value={login.name} onChange={handleChange}/>
-					
-					</div>
-					<div className="input-group form-group">
-						<div className="input-group-prepend">
-							<span className="input-group-text"><i className="fas fa-key"></i></span>
-						</div>
-						<input type="password" className="form-control" placeholder="password"  name="password" value={login.password} onChange={handleChange}/>
-						
-					</div>
-					<div class="row align-items-center remember">
-						<input type="checkbox"/>Recordarme
-						
-					</div>
-					<div class="form-group">
-						<input type="submit" value="Login"   disabled={(!error.name && !error.email)?false:true} className="btn float-right login_btn"/>
-						{error.password && (<p className="p" >{error.password}</p>)}
-						{error.name && (<p className="p">{error.name}</p>)}
-					</div>
-				</form>
-			</div>
-			<div className="footer">
-				
+    <div className="containerform">
+      <div class="container">
+        <div class="d-flex justify-content-center h-100">
+          <div class="card">
+            <div class="card-header">
+              <h3>Ingresa</h3>
+            </div>
+            <div class="cardbody">
+              <form onSubmit={handleLogin}>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-user"></i>
+                    </span>
+                  </div>
 
-            	 <div className="buttons mb-3">
-		
-					<FacebookLogin
-                                       appId="500036998618970"
-                                       fields="name,email,picture"
-                                       textButton="ingresa con facebook"
-                                       callback={responseFacebook}
-                                      cssClass="facebook"
-                                      icon={<i className="bi bi-facebook" style={{marginRight:'5px',marginLeft:'10px'}}> </i>}/>
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="email"
+                    name="name"
+                    value={login.name}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div className="input-group form-group">
+                  <div className="input-group-prepend">
+                    <span className="input-group-text">
+                      <i className="fas fa-key"></i>
+                    </span>
+                  </div>
+                  <input
+                    type="password"
+                    className="form-control"
+                    placeholder="password"
+                    name="password"
+                    value={login.password}
+                    onChange={handleChange}
+                  />
+                </div>
+                <div class="row align-items-center remember">
+                  <input type="checkbox" />
+                  Recordarme
+                </div>
+                <div class="form-group">
+                  <input
+                    type="submit"
+                    value="Login"
+                    disabled={!error.name && !error.email ? false : true}
+                    className="btn float-right login_btn"
+                  />
+                  {error.password && <p className="p">{error.password}</p>}
+                  {error.name && <p className="p">{error.name}</p>}
+                </div>
+              </form>
+            </div>
+            <div className="footer">
+              <div className="buttons mb-3">
+                <FacebookLogin
+                  appId="500036998618970"
+                  fields="name,email,picture"
+                  textButton="ingresa con facebook"
+                  callback={responseFacebook}
+                  cssClass="facebook"
+                  icon={
+                    <i
+                      className="bi bi-facebook"
+                      style={{ marginRight: "5px", marginLeft: "10px" }}
+                    >
+                      {" "}
+                    </i>
+                  }
+                />
 
-					<GoogleLogin
-                                                   clientId="704047841570-i6n8lvda54ako40u5173qcd8os0qfphk.apps.googleusercontent.com"
-                                                   buttonText="ingresa con google"
-                                                   onSuccess={responseGoogle}
-                                                    onFailure={responseGoogle} 
-                                                   cookiePolicy={'single_host_origin'}
-												   onChange={responseFacebook}/>
-												  
+                <GoogleLogin
+                  clientId="704047841570-i6n8lvda54ako40u5173qcd8os0qfphk.apps.googleusercontent.com"
+                  buttonText="ingresa con google"
+                  onSuccess={responseGoogle}
+                  onFailure={responseGoogle}
+                  cookiePolicy={"single_host_origin"}
+                  onChange={responseFacebook}
+                />
+              </div>
 
-										   
-				</div>
-
-				<div className="d-flex justify-content-center links ">
-				<Link to="/register">
-					No tienes cuenta?<a href="#">Registrarse</a>
-					</Link>
-				</div>
-				<div className="d-flex justify-content-center">
-					<a href="#">Olvidaste tu contraseña</a>
-				</div>
-			</div>
-		</div>
-	</div>
-	</div>
+              <div className="d-flex justify-content-center links ">
+                <Link to="/register">
+                  No tienes cuenta?<a href="#">Registrarse</a>
+                </Link>
+              </div>
+              <div className="d-flex justify-content-center">
+                <a href="#">Olvidaste tu contraseña</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
