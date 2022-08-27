ceac2427b91b086481454746400bb8b41555af90
Andreas Haller <andi_haller@hotmail.com>
2022-08-27 10:51:16 -0400

    fix error config

--- client/src/redux/configAut.js
+++ client/src/redux/configAut.js
@@ -1,6 +1,8 @@
 export function  authorization(){
     let usuario = JSON.parse(localStorage.getItem("usuario"));
     
+    if(!usuario) return;
+
     const config = {
         headers: {
             Authorization: usuario.token
