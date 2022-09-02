8b68419389e329371cd74166d951ae0382cdce90
mishino123 <jicorreac@unal.edu.co>
2022-09-02 13:21:35 -0500

    hola

--- api/src/routes/stripe.routes.js
+++ api/src/routes/stripe.routes.js
@@ -3,6 +3,19 @@ const { paymentStripe } = require("./controllers/stripe.controller.js");
 const {getUserInfo} = require("./controllers/user.controllers");
 const router = Router();
 
+const path = require('node:path');
+const fs = require('fs');
+const handlebars = require("handlebars");
+const filePath = path.join(__dirname, '../utils/templates/invoice.html');
+const source = fs.readFileSync(filePath, 'utf-8').toString();
+const template = handlebars.compile(source);
+const { MAIL_PORT} = process.env;
+const url=`${MAIL_PORT}/home`;
+
+
+
+
+
 router.post("/", async (req, res) => {
     try {
         const { userId, membershipId, membershipPrice, membershipType, dni, address, birthday } = req.body.info
@@ -13,6 +26,42 @@ router.post("/", async (req, res) => {
             res.status(400).json({ message: payment.message })
         } else {
             const user = await getUserInfo(userId);
+            const mail=user.dataValues.mail;
+            const init=payment.start_date;
+            const finish=payment.end_date;
+
+            const replacements = {
+                url:url,
+                name:user.dataValues.name,
+                membershipPrice: membershipPrice,
+                membershipType: membershipType,
+                init:init,
+                finish:finish,
+              
+            };
+            const htmlToSend = template(replacements);
+            mandarMail(
+                mail,
+                "Gracias por registrarte",
+                `Hola , felicitaciones, tomaste el primer paso a una vida más sana!`,
+                htmlToSend 
+              );
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
             return res.status(200).json(payment);
         }
     } catch (error) {
