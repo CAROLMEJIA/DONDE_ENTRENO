2680546a824867e4682b70f3101a63166a6ed13c
Andreas Haller <andi_haller@hotmail.com>
2022-09-03 18:32:26 -0400

    ructas back para rating - default image en DB

---
+++ api/src/routes/controllers/rating.controller.js
@@ -0,0 +1,93 @@
+const { Rating } = require("../../db/db.js");
+const { Activity } = require("../../db/db.js");
+
+async function getAllRatings() {
+  const activities = await Activity.findAll({
+    attributes: ["id"],
+    raw: true,
+  });
+  //console.log("Activities: ", activities);
+
+  const ratings = await Promise.all(
+    activities.map(async (act) => {
+      const rating = await Rating.findAll({
+        where: {
+          activityId: act.id,
+        },
+        attributes: ["value"],
+        raw: true,
+      });
+
+      return {
+        activityId: act.id,
+        rating: rating.map((val) => {
+          return val.value;
+        }),
+      };
+    })
+  );
+  //console.log("Ratings: ", ratings);
+
+  const ratingsPromediado = ratings.map((rat) => {
+    let aux = 0;
+    for (let i = 0; i < rat.rating.length; i++) {
+      aux = aux + rat.rating[i];
+    }
+
+    return {
+      activityId: rat.activityId,
+      rating: Math.round(
+        aux / (rat.rating.length === 0 ? 1 : rat.rating.length)
+      ),
+      votes: rat.rating.length,
+    };
+  });
+  //console.log("Promediado: ", ratingsPromediado);
+
+  return ratingsPromediado;
+}
+
+async function getActivityRating(id) {
+  const rating = await Rating.findAll({
+    where: {
+      activityId: id,
+    },
+    attributes: ["value"],
+    raw: true,
+  });
+
+  let aux = 0;
+  for (let i = 0; i < rating.length; i++) {
+    aux = aux + rating[i].value;
+  }
+
+  return {
+    activityId: Number(id),
+    rating: Math.round(
+      aux / (rating.length === 0 ? 1 : rating.length)
+    ),
+    votes: rating.length,
+  };
+
+}
+
+async function addActivityRating(activityId, userId, value) {
+  const rating = await Rating.create({
+    value: value,
+    activityId: activityId,
+    userId: userId,
+  });
+
+  return rating;
+}
+
+async function modifActivityRating(activityId, userId, value) {
+  return "EHHHHH, NOPE";
+}
+
+module.exports = {
+  getAllRatings,
+  getActivityRating,
+  addActivityRating,
+  modifActivityRating,
+};
---
+++ api/src/routes/rating.routes.js
@@ -0,0 +1,64 @@
+const { Router } = require("express");
+const router = Router();
+const {
+  getAllRatings,
+  getActivityRating,
+  addActivityRating,
+  modifActivityRating,
+} = require("./controllers/rating.controller.js");
+
+router.get("/", async (req, res) => {
+  try {
+    const rating = await getAllRatings();  // devuelve un array de objetos con id de actividad, valoracion y cantidad de votos
+
+    res.status(200).json(rating);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const rating = await getActivityRating(id);  // devuelve un objeto con id de actividad, valoracion y cantidad de votos
+
+    res.status(200).json(rating);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { activityId, userId, value } = req.body;
+
+    if (!activityId || !userId || !value) {
+      res.status(400).json({ message: "Faltan datos obligatorios" });
+    }
+
+    const rating = await addActivityRating(activityId, userId, value);
+
+    res.status(200).json(rating);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+router.put("/", async (req, res) => {    // Devuelve EHHHHH, NOPE
+  try {
+    //const { id } = req.params;   // Id de la puntuacion especifica del usuario ??
+    const { activityId, userId, value } = req.body;
+
+    if (!activityId || !userId || !value) {
+      res.status(400).json({ message: "Faltan datos obligatorios" });
+    }
+
+    const rating = await modifActivityRating(activityId, userId, value);
+
+    res.status(200).json(rating);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
+});
+
+module.exports = router;
