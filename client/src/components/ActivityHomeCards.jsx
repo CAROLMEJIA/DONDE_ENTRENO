284b60ffa90b89f4cb7ae27df9f445d61bd23d26
marko <markoschneider34@gmail.com>
2022-09-06 10:39:04 -0300

    cartasHome

--- client/src/components/ActivityHomeCards.jsx
+++ client/src/components/ActivityHomeCards.jsx
@@ -3,24 +3,25 @@ import ActivityCard from "./ActivityCard.jsx";
 import { useSelector } from "react-redux";
 import './estilos/AHC.css'
 
-const AHC = ({userls}) => {
-    
+const AHC = ({ userls }) => {
+
     const activities = useSelector((state) => state.activities);
     const activitiesHome = activities.slice(0, 3)
 
     return (
-        <div className = 'Cards-Activity'>
+        <div className='Cards-Activity'>
             {activitiesHome?.map(act => (
                 <ActivityCard
-                    userls = {userls}
-                    key = {act.id}
-                    name = {act.name}
-                    image = {act.image}
-                    description = {act.description}
+                    key={act.id}
+                    userls={userls}
+                    id={act.id}
+                    name={act.name}
+                    image={act.image}
+                    description={act.description}
                 />
             ))}
         </div>
-    ) 
+    )
 };
 
 export default AHC
